7565c5538295514784f06c5cb7de50f5e2fef25b
ayansheikh6600 <ayansheikh6600@gmail.com>
2023-10-24 11:12:14 +0500

    some changes & db connect

--- app.js
+++ app.js
@@ -1,21 +1,117 @@
 require("dotenv").config();
 
-const express = require("express")
+const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const UserModel = require("./model/userSchema");
 // const PORT = 5000;
 
+//body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/",(req, res)=>{
+const DB_URI = process.env.DB_URI;
+mongoose.connect(DB_URI);
+mongoose.connection.on("connected", () => console.log("MongoDB Connected"));
+mongoose.connection.on("error", (err) => console.log("MongoDB Error", err));
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "serverup",
+  });
+});
+
+// login Api
+app.post("/api/signup", async (req, res) => {
+  try {
+    const body = req.body;
+    const { firstName, lastName, age, gender, email, password, phoneNo } = body;
+    if (
+      !firstName ||
+      !lastName ||
+      !age ||
+      !gender ||
+      !email ||
+      !password ||
+      !phoneNo
+    ) {
+      res.json({
+        status: false,
+        message: "Required fields are missing",
+        data: null,
+      });
+      return;
+    }
+    const hashpass = await bcrypt.hash(password, 10);
+    const objToSend = {
+      first_name: firstName,
+      last_name: lastName,
+      phone_no: phoneNo,
+      age,
+      gender,
+      email,
+      password: hashpass,
+    };
+    const emailExist = await UserModel.findOne({ email });
+    console.log(emailExist, "emailExist");
+    if (emailExist) {
+      res.json({
+        status: false,
+        message: "this email address has been already exists Please try again",
+        data: null,
+      });
+      return;
+    }
+    const userSave = await UserModel.create(objToSend);
+
     res.json({
-        message : "serverup"
-    })
-})
+      status: true,
+      message: "user successfully signup",
+      data: userSave,
+    });
+  } catch (error) {
+    res.json({
+      status: false,
+      message: error.message,
+      data: null,
+    });
+  }
+});
 
+app.post("/api/login", async (req, res) => {
+  const { email, password } = req.body;
+  
+  const emailExist = await UserModel.findOne({email})
+  if (!emailExist) {
+    res.json({
+      message: "Invalid Credential",
+      status: false,
+      data: null,
+    });
+    return;
+  }
+  const comparePass = await bcrypt.compare(password, emailExist.password);
+  if (comparePass) {
+    res.json({
+      message: "user login",
+      status: true,
+      data: emailExist,
+    });
+    return;
+  } else {
+    res.json({
+      message: "Invalid Credential",
+      status: false,
+      data: null,
+    });
+    return;
+  }
+});
 
-const globalPORT = process.env.PORT
+const globalPORT = process.env.PORT;
 // console.log(process.env)
 
-app.listen(globalPORT, ()=>{
-    console.log(`server running on localhost:${globalPORT}`)
-})
+app.listen(globalPORT, () => {
+  console.log(`server running on localhost:${globalPORT}`);
+});
---
+++ model/userSchema.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const schema = new mongoose.Schema({
+  first_name: {
+    type: String,
+    required: true,
+  },
+  last_name: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  gender: {
+    type: String,
+    required: true,
+  },
+  phone_no: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const UserModel = mongoose.model("user", schema);
+
+module.exports = UserModel;
